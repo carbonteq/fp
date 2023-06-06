4e282dea32b1be72fe48790af2c913123e6354fb
Arslan <rslnkrmt2552@gmail.com>
2023-06-06 05:52:08 +0500

    init commit

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// https://jestjs.io/docs/configuration
+
+/** @type {require('@jest/types').Config.InitialOptions} */
+const config = {
+	moduleFileExtensions: ["js", "ts"],
+	testEnvironment: "node",
+	notify: false,
+	rootDir: ".",
+	roots: ["src", "tests"],
+	testRegex: ".*\\.(spec|test|integration)\\.ts",
+	testPathIgnorePatterns: ["/node_modules/"],
+	transform: {
+		"^.+\\.(t|j)sx?$": "@swc/jest",
+	},
+	transformIgnorePatterns: ["/node_modules/"],
+	moduleNameMapper: {
+		"^@carbonteq/fp(.*)$": ["<rootDir>/lib"],
+	},
+	setupFilesAfterEnv: ["jest-extended/all"],
+};
+
+module.exports = config;
---
+++ rollup.config.ts
@@ -0,0 +1,47 @@
+import type { RollupOptions } from "rollup";
+
+import { swc, defineRollupSwcOption } from "rollup-plugin-swc3";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
+import externals from "rollup-plugin-node-externals";
+
+const config: RollupOptions =
+	// CommonJS (for Node) & ESM (for bundlers)
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "lib/index.js",
+				format: "cjs",
+				sourcemap: false,
+			},
+			{
+				file: "lib/index.mjs",
+				format: "es",
+				sourcemap: false,
+			},
+			// { file: pkg.main, format: 'cjs', sourcemap: false },
+			// { file: pkg.module, format: 'es', sourcemap: false },
+		],
+		plugins: [
+			typescriptPaths({ preserveExtensions: true }),
+			externals(),
+			swc(
+				defineRollupSwcOption({
+					minify: true,
+					tsconfig: "./tsconfig.build.json",
+					jsc: {
+						minify: {
+							mangle: false,
+							compress: true,
+						},
+						experimental: {
+							keepImportAssertions: true,
+						},
+					},
+				}),
+			),
+		],
+		// external: ['oxide.ts', 'oxide.ts/dist/core', 'node:crypto'],
+	};
+
+export default config;
