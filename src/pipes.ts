28d814dc907311b20679acb5ac90ef61fb3483ce
ahmed-deftoner <ahmedghwthst786@gmail.com>
2024-02-13 19:49:17 +0500

    fix: type check input funcs

--- src/pipes.ts
+++ src/pipes.ts
@@ -1,8 +1,25 @@
 type Fn = (...args: any[]) => any;
 
-type LastReturnType<L extends Fn[]> = L extends [...any, infer Last extends Fn]
-  ? ReturnType<Last>
-  : never;
+interface Pipe {
+    <A>(value: A): A;
+    <A, B>(value: A, fn1: (input: A) => B): B;
+    <A, B, C>(value: A, fn1: (input: A) => B, fn2: (input: B) => C): C;
+    <A, B, C, D>(
+        value: A,
+        fn1: (input: A) => B,
+        fn2: (input: B) => C,
+        fn3: (input: C) => D
+    ): D;
+    <A, B, C, D, E>(
+        value: A,
+        fn1: (input: A) => B,
+        fn2: (input: B) => C,
+        fn3: (input: C) => D,
+        fn4: (input: D) => E
+    ): E;
+}
 
-export const pipe = <Funcs extends Fn[]>(value: any, ...fns: Funcs) =>
-  fns.reduce((acc, fn) => fn(acc), value) as LastReturnType<Funcs>;
+export const pipe: Pipe = (value: any, ...fns: Function[]): unknown => {
+    return fns.reduce((acc, fn) => fn(acc), value);
+};
+  
