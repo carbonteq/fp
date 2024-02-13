638ef6375d618b8cd8a7b5465edf555b1b2f1fc0
ahmed-deftoner <ahmedghwthst786@gmail.com>
2024-02-13 19:40:08 +0500

    fix: last return tye

--- src/pipes.ts
+++ src/pipes.ts
@@ -1,2 +1,8 @@
-export const pipe = (value: any, ...fns: Function[]) =>
-  fns.reduce((acc, fn) => fn(acc), value);
+type Fn = (...args: any[]) => any;
+
+type LastReturnType<L extends Fn[]> = L extends [...any, infer Last extends Fn]
+  ? ReturnType<Last>
+  : never;
+
+const pipe = <Funcs extends Fn[]>(value: any, ...fns: Funcs) =>
+  fns.reduce((acc, fn) => fn(acc), value) as LastReturnType<Funcs>;
