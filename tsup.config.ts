c407a96a87f756eccf6456790a5d429cb80eb8fd
Arslan <rslnkrmt2552@gmail.com>
2025-02-10 16:49:33 +0100

    update option implementation to use async chains

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/**/*.ts"],
+  entry: ["./src/**/*.ts", "!./src/**/*_old.ts"],
   format: ["esm", "cjs"],
   sourcemap: true,
   clean: true,
