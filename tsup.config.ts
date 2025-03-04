1df78edd01a7107e9302f994b57da0c63645f1c5
Arslan <rslnkrmt2552@gmail.com>
2025-03-04 13:15:05 +0100

    update build cfg

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
+export default defineConfig((_options) => ({
   entry: ["./src/**/*.ts", "!./src/**/*_old.ts"],
   format: ["esm", "cjs"],
   sourcemap: true,
@@ -8,6 +8,15 @@ export default defineConfig({
   target: "node20",
   outDir: "dist",
   minify: false,
+  splitting: true,
+  treeshake: true,
   bundle: false,
   dts: true,
-});
+  esbuildOptions(opts, _ctx) {
+    opts.drop = process.env.NODE_ENV === "production" ? ["console"] : [];
+  },
+  // pure:
+  //   process.env.NODE_ENV === "production"
+  //     ? ["console.log", "console.debug", "console.info"]
+  //     : [],
+}));
